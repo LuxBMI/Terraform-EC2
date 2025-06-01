23b59f154adbd8f5ef3f926f692750a163879f82
LuxBMI <214187760+LuxBMI@users.noreply.github.com>
2025-06-01 10:51:52 -0500

    Modularize TF config: moved provider and resource blocks

--- main.tf
+++ main.tf
@@ -8,15 +8,4 @@ terraform {
    required_version = ">= 1.5.0"
 }
 
-provider "aws" {
-  region = "us-east-1"
-}
 
-resource "aws_instance" "LuxBMImini1" {
-ami           = "ami-0c101f26f147fa7fd" # Amazon Linux 2023, us-east-1
-instance_type = "t2.micro"
-
- tags = {
-  Name = "ExampleInstance"
- }
-}
